c5f7967292c7b356e07c51086d1f6976d7aab9ec
Teodor Lupascu <teodor.lupascu@dynamicventures.com>
2021-03-31 13:27:36 +0300

    * Replace label with a button

--- TestApp/TestApp/ViewController.swift
+++ TestApp/TestApp/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func didTouchButton(_ sender: UIButton) {
+        let message = UIAlertController(title: "Hello World", message: "Hi everyone! This is a sample", preferredStyle: .alert)
+        self.present(message, animated: true)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
